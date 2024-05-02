c70b09348974a4413f009a7ac3b1554ab71b895c
cnrprod-team-65719 <toylenium@mail.ru>
2024-05-02 20:19:53 +0800

    fix3

--- out_back_with_bad_db/out/olddb/serializers.py
+++ out_back_with_bad_db/out/olddb/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework.serializers import ModelSerializer, StringRelatedField
 from olddb.models import Companies, Faculty
-from base.models import Speciality, Theme
+from base.models import Speciality#, Theme
 
 
-class CompanySerializer(ModelSerializer):
-    themes = StringRelatedField(many=True, read_only=True)
+# class CompanySerializer(ModelSerializer):
+#     themes = StringRelatedField(many=True, read_only=True)
 
-    class Meta:
-        model = Companies
-        fields = "__all__"
+#     class Meta:
+#         model = Companies
+#         fields = "__all__"
 
 
 class FacultySerializer(ModelSerializer):
